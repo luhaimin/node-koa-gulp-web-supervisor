e4ce80ae5b6a22ab416faa4efd38032a9a745cbd
luhaimin <lu_html@163.com>
2019-11-01 16:11:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,9 +2,6 @@ const hostName = process.env.NODE_ENV === "production"
     ? "https://www.xxxxx.com"
     : "https://www.yyyy.com"      //TODO 测试环境（因目前无测试环境）
 
-// https://www.yujiso.com/api/sites
-//https://www.skybaymall.com
-//https://ceshi.api.vipsouq.net
 module.exports = {
     port: 3000,
     imgBasePath: "http://localhost:3000/img",            //图片域（只供图片URL拼接时使用）
